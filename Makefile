cca88df8cb1e32cc38a45c193890dc2191cab7ac
pxseu <57842793+pxseu@users.noreply.github.com>
2022-05-04 15:06:47 +0200

    feat: logging

--- Makefile
+++ Makefile
@@ -15,12 +15,12 @@ build:
 	MIX_ENV=${BUILD_ENV} mix release
 
 uninstall:
-	systemctl stop ${APP_NAME}.service
-	systemctl disable ${APP_NAME}.service
+	systemctl stop ${APP_NAME}.service || true
+	systemctl disable ${APP_NAME}.service || true
 	rm -rf /etc/systemd/system/${APP_NAME}.service
 	rm -rf /opt/${APP_NAME}
 
-install: uninstall
+install: uninstall build
 	cp ./_build/prod/rel/${APP_NAME}/ /opt/${APP_NAME} -r
 	cp ./${APP_NAME}.service /etc/systemd/system/
 	systemctl daemon-reload
--- lib/ganyu.ex
+++ lib/ganyu.ex
@@ -6,18 +6,30 @@ defmodule Ganyu do
 
   use Application
 
+  require Logger
+
   def start(_type, _args) do
+    Logger.log(:info, "Starting Ganyu...")
+
     children = [
       Plug.Cowboy.child_spec(
         scheme: :http,
         plug: Ganyu.Router,
-        options: [port: Application.get_env(:ganyu, :port, 8080)]
+        options: [port: get_port()]
       ),
       Ganyu.Database.Postgres.child_spec([])
     ]
 
     opts = [strategy: :one_for_one, name: __MODULE__.Supervisor]
 
-    Supervisor.start_link(children, opts)
+    {:ok, pid} = Supervisor.start_link(children, opts)
+
+    Logger.log(:info, "Ganyu is running on http://localhost:#{get_port()}")
+
+    {:ok, pid}
+  end
+
+  defp get_port() do
+    Application.get_env(:ganyu, :port, 8080)
   end
 end
