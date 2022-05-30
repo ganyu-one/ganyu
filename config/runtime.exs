801543e326f88f67eacdd02f17631f1907039fe8
Kuba Ellwart <57842793+pxseu@users.noreply.github.com>
2022-05-30 23:27:24 +0200

    feat: port

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,7 +2,7 @@ import Config
 
 if config_env() == :prod do
   config :ganyu,
-    port: String.to_integer(System.get_env("GANYU_PORT", "8080")),
+    port: String.to_integer(System.get_env("PORT", "8080")),
     proxy_path: System.get_env("GANYU_PROXY", "https://pximg.pxseu.com"),
     postgres_hostname:
       System.get_env(
