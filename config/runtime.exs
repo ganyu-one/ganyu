57178441ce4da9bb44be9c5165c56fea92b16d89
pxseu <57842793+pxseu@users.noreply.github.com>
2024-08-18 16:48:15 +0900

    fix runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,7 +3,7 @@ import Config
 if config_env() == :prod do
   config :ganyu,
     port: String.to_integer(System.get_env("PORT", "8080")),
-    : System.get_env("GANYU_PROXY", "https://pximg.pxseu.com"),
+    proxy_path: System.get_env("GANYU_PROXY", "https://pximg.pxseu.com"),
     postgres_hostname:
       System.get_env(
         "GANYU_POSTGRES_HOSTNAME",
