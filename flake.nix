3242a2669cb51d54e53c324e9f5ca9b1dadbc635
pxseu <57842793+pxseu@users.noreply.github.com>
2024-08-18 16:40:31 +0900

    start nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Elixir's application";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        erl = pkgs.beam.packages.erlang;
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages =
+            [
+              erl.elixir
+              erl.elixir-ls
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+              pkgs.darwin.apple_sdk.frameworks.CoreServices
+            ];
+        };
+      }
+    );
+}
